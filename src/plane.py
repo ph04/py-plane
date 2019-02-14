dc259fb2809709f2e740fd02a27738dde337095e
ph04 <47356461+ph04@users.noreply.github.com>
2019-02-14 18:20:00 +0100

    Update plane.py

--- src/plane.py
+++ src/plane.py
@@ -1,7 +1,11 @@
 from math import sqrt
+import equation
 import system
 
 class Triangle:
+    """
+    Class for triangles defined by three points on a plane.
+    """
     def __init__(self, xA, xB, xC, yA, yB, yC):
         self.xA = xA
         self.xB = xB
@@ -40,14 +44,19 @@ class Triangle:
         return (self.xA + self.xB + self.xC) / 3, (self.yA + self.yB + self.yC) / 3
 
 class Parabola:
+    """
+    Class for parabolas on a plane.
+    """
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
+        if not self.a:
+            raise ValueError("The value of 'a' can't be 0.")
     
     def vertex(self):
         """
-        Returns vertex's coordinates.
+        Returns vertex' coordinates.
         """
         return -self.b / (2 * self.a), (4 * self.a * self.c - self.b**2) / (4 * self.a)
 
@@ -61,9 +70,24 @@ class Parabola:
         """
         Returns axis' equation.
         """
-        return -self.b / (2 * self.a)
+        return self.vertex()[0]
+
+    def x_value(self, y):
+        """
+        Returns the x value for the given y.
+        """
+        return equation.SecondGradeEquation(self.a, self.b, self.c - y).solve()
+    
+    def y_value(self, x):
+        """
+        Returns the y value for the given x.
+        """
+        return self.a * x**2 + self.b * x + self.c
 
 class Circumference:
+    """
+    Class for circumferences on a plane.
+    """
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
@@ -80,6 +104,77 @@ class Circumference:
         Returns radius' length.
         """
         return 0.5 * sqrt((self.a**2) + (self.b**2) - (4 * self.c))
+    
+    def x_value(self, y):
+        """
+        Returns the x value for the given y.
+        """
+        return equation.SecondGradeEquation(1, self.a, y**2 + self.b * y + self.c).solve()
+    
+    def y_value(self, x):
+        """
+        Returns the y value for the given x.
+        """
+        return equation.SecondGradeEquation(1, self.b, x**2 + self.a * x + self.c).solve()
+
+class Ellipse:
+    """
+    Class for ellipses on a plane.
+    """
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.c = self.a**2 - self.b**2
+        if not self.a or not self.b:
+            raise ValueError("Nor the value of 'a' neither the value of 'b' can be 0.")
+    
+    def first_focus(self):
+        """
+        Returns first focus' coordinates.
+        """
+        return -sqrt(self.c), 0
+    
+    def second_focus(self):
+        """
+        Returns second focus' coordinates.
+        """
+        return sqrt(self.c), 0
+
+    def first_vertex(self):
+        """
+        Returns first vertex' coordinates.
+        """
+        return self.a, 0
+
+    def second_vertex(self):
+        """
+        Returns second vertex' coordinates.
+        """
+        return 0, self.b
+
+    def third_vertex(self):
+        """
+        Returns third vertex' coordinates.
+        """
+        return -self.a, 0
+
+    def fourth_vertex(self):
+        """
+        Returns fourth vertex' coordinates.
+        """
+        return 0, -self.b
+    
+    def x_value(self, y):
+        """
+        Returns the x value for the given y.
+        """
+        return equation.SecondGradeEquation(self.b**2, 0, self.a**2 * y**2 - (self.a * self.b)**2).solve()
+    
+    def y_value(self, x):
+        """
+        Returns the y value for the given x.
+        """
+        return equation.SecondGradeEquation(self.a**2, 0, self.b**2 * x**2 - (self.a * self.b)**2).solve()
 
 def triangle():
     xa = float(input("Enter xA: "))
@@ -117,12 +212,26 @@ def circumference():
     print(f"Center is {circ.center()}")
     print(f"Radius is {circ.radius()}")
 
+def ellipse():
+    a = float(input("Enter a: "))
+    b = float(input("Enter b: "))
+
+    elli = Ellipse(a, b)
+
+    print(f"First focus is {elli.first_focus()}")
+    print(f"Second focus is {elli.second_focus()}")
+    print(f"First vertex is {elli.first_vertex()}")
+    print(f"Second vertex is {elli.second_vertex()}")
+    print(f"Third vertex is {elli.third_vertex()}")
+    print(f"Fourth vertex is {elli.fourth_vertex()}")
+
 def main():
     print("Choose your graph: ")
     print("1) Triangle")
     print("2) Parabola")
     print("3) Circumference")
-    
+    print("4) Ellipse")
+
     choice = input()
 
     if choice == "1":
@@ -131,6 +240,8 @@ def main():
         parabola()
     elif choice == "3":
         circumference()
+    elif choice == "4":
+        ellipse()
     else:
         raise ValueError("Invalid choice.")
 
